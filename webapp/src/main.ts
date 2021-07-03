f83044d18c925a6c4e45c0d29a919ea65efcb325
Sergei Dianov <disevsoft@gmail.ru>
2021-07-03 14:32:59 +0300

    ere

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -5,6 +5,7 @@ import router from "./router";
 import store from "./store";
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
+import '@/global/styles.css';
 const app = createApp(App)
 app.use(store)
 app.use(router)

3c50e273fa3956424f0b56772f951661aafd362a
Sergei Dianov <disevsoft@gmail.com>
2021-09-07 17:01:47 +0300

    asdf

---
+++ webapp/src/catalogs/nomenclature/nomenclatureListForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        номенклатура
+    </div>
+</template>
+
+<script>
+import { defineComponent, ref, watch, onMounted } from 'vue';
+
+export default defineComponent({
+    components: {     
+    },
+    
+    props: {
+        
+    },
+    setup(props, { emit }) {
+       return { };
+    } 
+  });
+</script>
+
+<style lang="scss">
+
+</style>
